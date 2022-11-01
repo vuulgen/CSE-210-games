0dea19b88328fba4217f94411a62a650817b210d
Brendan Willis <brendanwillis@me.com>
2022-11-01 10:19:52 -0600

    My first commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace helloworld
+{
+    public class Program{
+        public static void Main(string[] args)
+        {
+             Console.WriteLine("What is your first name?");
+             string first = Console.ReadLine();
+            Console.WriteLine("What is your last name?");
+             string last = Console.ReadLine();
+
+            Console.WriteLine($"Your name is, {last}, {first}{last}");
+
+        }
+
+    }
+
+}
+
+
+
